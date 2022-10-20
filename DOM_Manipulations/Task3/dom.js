6d47b10082b5fb3ccdce14a684c3028570a9d6ed
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2022-10-20 17:45:58 +0530

    first commit

---
+++ DOM_Manipulations/Task3/dom.js
@@ -0,0 +1,8 @@
+document.title = 'Item Listener modified'; //modify the title 
+
+let headerTitle = document.getElementById('header-title');
+headerTitle.style.borderBottom = 'solid 3px #000'; //add black border to header title
+
+let header = document.getElementById('title');
+header.style.color = 'green'; //change font color of title to green
+header.style.fontWeight = 'bold'; //make title bold
