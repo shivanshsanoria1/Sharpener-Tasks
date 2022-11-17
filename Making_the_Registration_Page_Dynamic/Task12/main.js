5242937b71abdc4d89bcd84f5ba9f83b9d097c42
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2022-11-17 14:25:06 +0530

    done with task 12

---
+++ Making_the_Registration_Page_Dynamic/Task12/main.js
@@ -0,0 +1,91 @@
+const form = document.getElementById('my-form');
+const userList = document.getElementById('users'); //ul
+
+/* FUNCTIONS */
+
+//store user details
+function storeUserDetails(e){
+    e.preventDefault();
+    //get userName, userEmail, user phone number inputs and store them in an user object
+    const userObj ={
+        name: document.getElementById('name').value, //userName
+        email: document.getElementById('email').value, //userEmail
+        phoneNumber: document.getElementById('phoneNumber').value //user phone number
+    };
+    //clear the input fields
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phoneNumber').value = '';
+    //add the user to crudcrud.com
+    axios
+    .post("https://crudcrud.com/api/600ce0b3d94145daa9f831310469a028/appointmentData",userObj)
+    .then((response) => {
+        displayUser(response.data);
+    })
+    .catch((err) => {
+        console.log(err);
+        displayError();
+    });
+}
+
+//display all the users stored in crudcrud.com when webpage is reloaded
+function displayAllUsers(){
+    axios
+    .get("https://crudcrud.com/api/600ce0b3d94145daa9f831310469a028/appointmentData")
+    .then((response) => {
+        response.data.forEach((userObj) => {
+            displayUser(userObj);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        displayError();
+    });
+}
+
+//display user stored in crudcrud.com based on the user object passed
+function displayUser(userObj){
+    //create new list item
+    const li = document.createElement('li');
+    li.className = 'item';
+    //append textnode with the value of userName, userEmail, user phone number to li
+    li.appendChild(document.createTextNode(userObj.name + ' : ' + userObj.email + ' : ' + userObj.phoneNumber));
+    
+    //create edit button
+    const editBtn = document.createElement('button');
+    editBtn.className = 'edit';
+    //append textnode to edit button
+    editBtn.appendChild(document.createTextNode('Edit'));
+    //append edit button to li
+    li.appendChild(editBtn);
+
+    //create delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'delete';
+    //append textnode to delete button
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    //append delete button to li
+    li.appendChild(deleteBtn);
+
+    //append li to ul
+    userList.appendChild(li);
+}
+
+//displays error message in DOM
+function displayError(){
+    //create new list item
+    const li = document.createElement('li');
+    li.className = 'error';
+    //append textnode showing error to li
+    li.appendChild(document.createTextNode('Error: something went wrong :('));
+
+    //append li to ul
+    userList.appendChild(li);
+}
+
+/* EVENT LISTENERS */
+
+//display all users event
+window.addEventListener('DOMContentLoaded',displayAllUsers);
+//Form submit event
+form.addEventListener('submit',storeUserDetails);
