3eb7a72dd57cbb0648b74c0ada628fcfc210a7e1
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-01-07 18:36:35 +0530

    added body parser

---
+++ Starting_with_Express_Framework/Task3/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+    <form action="/product" method="POST">
+        <label for="title">Product:</label>
+        <input type="text" id="title" name="title"><br>
+        <label for="size">Size:</label>
+        <input type="number" id="size" name="size"><br>
+        <button type="submit">Add Product</button>
+    </form>
+    `);
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Homepage</h1>');
+});
+
+app.listen(3000);
