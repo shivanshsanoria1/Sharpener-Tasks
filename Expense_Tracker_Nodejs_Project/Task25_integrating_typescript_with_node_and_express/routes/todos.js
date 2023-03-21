5b3707dd0962ede90964a96f9c663de737bd918d
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-03-21 15:09:25 +0530

    typescript with node and express

---
+++ Expense_Tracker_Nodejs_Project/Task25_integrating_typescript_with_node_and_express/routes/todos.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json(todos);
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ msg: 'Added todo', todo: newTodo, todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = {
+            id: todos[todoIndex].id,
+            text: req.body.text
+        };
+        return res.status(200).json({ msg: 'updated todo', todos });
+    }
+    res.status(404).json({ msg: 'No todo found with this id' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const newTodos = todos.filter((todoItem) => todoItem.id !== tid);
+    if (newTodos.length === todos.length) {
+        return res.status(404).json({ msg: 'No todo found with this id' });
+    }
+    todos = [...newTodos];
+    res.status(200).json({ msg: 'Todo deleted', todos });
+});
+exports.default = router;
