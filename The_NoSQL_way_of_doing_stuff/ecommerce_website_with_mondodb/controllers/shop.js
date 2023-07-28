2f8fa6c1a13720953fa0b336c6845c2ff1138004
shivanshsanoria1 <shivanshsanoria1@gmail.com>
2023-07-28 16:41:03 +0530

    Getting Orders Placed

--- The_NoSQL_way_of_doing_stuff/ecommerce_website_with_mondodb/controllers/shop.js
+++ The_NoSQL_way_of_doing_stuff/ecommerce_website_with_mondodb/controllers/shop.js
@@ -86,44 +86,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
+    //let fetchedCart;
+    req.user
+    .addOrder()
+    .then(() => {
+        res.redirect('/orders');
     })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch(err => console.log(err));
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null);
-    })
-    .then(result => {
-      res.redirect('/orders');
-    })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders({include: ['products']})
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
+    req.user
+    .getOrders()
+    .then((orders) => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
+        });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
